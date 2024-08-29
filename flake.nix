725eb0a3d88efc5df60045b29f7871cce27afaf9
Lucas Givens <lucas.givens@proton.me>
2024-08-29 19:18:00 -0400

    testing

--- flake.nix
+++ flake.nix
@@ -1,64 +1,65 @@
 {
   description = "nn in rust";
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
   };
-
   outputs = { self, nixpkgs }: 
   let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs{
-      inherit system;
-    };
+    systems = [ "x86_64-linux" "riscv64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-      name = "rs-mnist";
-      version = "1.0.0";
-      cargoLock = {
-        lockFile = ./Cargo.lock;
-      };
-      src = self;
-      buildInputs = [
-        pkgs.rustc
-        pkgs.cargo
-        pkgs.rust-analyzer
-        pkgs.lldb_9
-        pkgs.gdb
-        pkgs.pkg-config
-        pkgs.systemd
-      ];
+    packages = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.rustPlatform.buildRustPackage {
+          name = "rs-mnist";
+          version = "1.0.0";
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          src = self;
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.lldb_9
+            pkgs.gdb
+            pkgs.pkg-config
+          ];
+          nativeBuildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.lldb_9
+            pkgs.gdb
+            pkgs.pkg-config
+          ];
+        };
+      }
+    );
 
-      nativeBuildInputs = [
-        pkgs.rustc
-        pkgs.cargo
-        pkgs.rust-analyzer
-        pkgs.lldb_9
-        pkgs.gdb
-        pkgs.pkg-config
-        pkgs.systemd
-        pkgs.udev
-      ];
-    };
-
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.rustc
-        pkgs.cargo
-        pkgs.rust-analyzer
-        pkgs.lldb_9
-        pkgs.gdb
-        pkgs.pkg-config
-        pkgs.systemd
-        pkgs.udev
-      ];
-
-      shellHook = ''
-      echo "dev"
-      RUST_BACKTRACE=1 test="dev" $SHELL
-      echo "exiting dev"
-      exit
-      '';
-    };
+    devShells = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.lldb_9
+            pkgs.gdb
+            pkgs.pkg-config
+          ];
+          shellHook = ''
+          echo "dev"
+          RUST_BACKTRACE=1 test="dev" $SHELL
+          echo "exiting dev"
+          exit
+          '';
+        };
+      }
+    );
   };
 }
